e0292bd0ef4fa4be0faa5ade9e4fafc5a10a123c
Andres Zapata <me@azapata.io>
2022-09-29 22:34:09 +0200

    Finish up

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const lorem = [
 
 const getIndex = (length) => Math.round(Math.random() * length)
 
-const List = () => {
+const List = (length = 10) => {
   return (
     <ul>
       {Array.from({ length: 10 }, (_, i) => (
@@ -42,6 +42,27 @@ const List = () => {
       ))}
     </ul>
   )
+}
+
+function areElementsCollidingVertically(el1, el2) {
+  const rect1 = el1.getBoundingClientRect()
+  const rect2 = el2.getBoundingClientRect()
+
+  console.info({ rect1, rect2 })
+
+  return (
+    rect1.top + rect1.height >= rect2.top &&
+    rect2.top + rect2.height >= rect1.top
+  )
+}
+
+function isElementAboveAnother(el1, el2) {
+  const rect1 = el1.getBoundingClientRect()
+  const rect2 = el2.getBoundingClientRect()
+
+  const result = rect1.top < rect2.top
+
+  console.info(result)
 }
 
 const options = {
@@ -124,40 +145,53 @@ function App() {
   const chartWrapperRef = useRef(null)
   const chartComponentRef = useRef(null)
   const actionsRef = useRef(null)
+  const rightBlockRef = useRef(null)
+  const leftBlockRef = useRef(null)
 
   useEffect(() => {
-    let options = {
-      root: chartWrapperRef.current,
-      rootMargin: "0px",
-      threshold: 1.0,
-    }
+    const elCards = document.getElementsByClassName("card")
 
-    function callback(entries, observer) {
-      console.log({ entries, observer })
-
+    const watchContainer = (entries) => {
       entries.forEach((entry) => {
-        console.log({ entry })
+        entry.target.style.opacity = entry.isIntersecting ? "1" : "0"
       })
     }
 
-    let observer = new IntersectionObserver(callback, options)
+    const observer = new IntersectionObserver(watchContainer, {
+      rootMargin: "20%", // for some reason this isn't working on codepen...
+      threshold: 0,
+    })
 
-    return () => {
-      // cleanup
+    observer.observe(elCards[0])
+    observer.observe(elCards[1])
+    observer.observe(elCards[2])
+    observer.observe(elCards[3])
+  }, [rightBlockRef?.current])
+
+  useEffect(() => {
+    if (chartWrapperRef?.current && actionsRef?.current) {
+      isElementAboveAnother(actionsRef.current, chartWrapperRef.current)
     }
-  }, [chartWrapperRef?.current])
+  }, [chartWrapperRef?.current, actionsRef?.current])
 
   return (
     <div className="App">
-      <div className="LeftBlock">
+      <div className="LeftBlock" ref={leftBlockRef}>
         <h3>Hello World</h3>
 
-        <div ref={chartWrapperRef}>
+        <div className="ChartWrapper" ref={chartWrapperRef}>
           <HighchartsReact
             highcharts={Highcharts}
             options={options}
             ref={chartComponentRef}
           />
+          <div>
+            <ol>
+              <li>Nec dubitamus multa iter quae et nos invenerat.</li>
+              <li>A communi observantia non est recedendum.</li>
+              <li>Unam incolunt Belgae, aliam Aquitani, tertiam.</li>
+            </ol>
+          </div>
         </div>
 
         <div className="Buttons" ref={actionsRef}>
@@ -166,10 +200,20 @@ function App() {
         </div>
       </div>
       <div className="MiddleBlock"></div>
-      <div className="RightBlock">
+      <div className="RightBlock" ref={rightBlockRef}>
         {Array.from({ length: 10 }, (_, i) => (
           <List key={i} />
         ))}
+        <h1>Hello! Please scroll down</h1>
+        <div className="spacer"> A white space...</div>
+        <div className="cards">
+          <div className="card debug-0">One</div>
+          <div className="card debug-1">Two</div>
+          <div className="card debug-2">Three</div>
+          <div className="card debug-3">Four</div>
+        </div>
+        <div className="spacer"> A white space...</div>
+        The end.
       </div>
     </div>
   )
